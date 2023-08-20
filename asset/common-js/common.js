9def84edd9a73fec31fbaed5f0a4d86bbcb0c0b5
Jahangir Nabin <jahangirnabin2@gmail.com>
2023-08-20 23:25:28 +0600

    health care issue

--- asset/common-js/common.js
+++ asset/common-js/common.js
@@ -1,12 +1,12 @@
-function toggleNavList(){
+function toggleNavList(fromDeepDirectory = false){
     let element = document.getElementById('navList');
     let menuToggleImg = document.getElementById('menuToggleImg');
     element.classList.toggle('collapsed');
     setTimeout(() => {
         if(element.classList.contains('collapsed')) {
-            menuToggleImg.src="./asset/img/Menu-icon.svg";
+            menuToggleImg.src= fromDeepDirectory ? "../asset/img/Menu-icon.svg" : "./asset/img/Menu-icon.svg";
         } else {
-            menuToggleImg.src="./asset/img/Close-Icons.svg";
+            menuToggleImg.src=fromDeepDirectory ? "../asset/img/Close-Icons.svg" : "./asset/img/Close-Icons.svg";
         }
     }, 600);
 }
